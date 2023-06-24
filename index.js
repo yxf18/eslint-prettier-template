734e1845d28a7546a223d219395e6ecc452db7af
杨雪枫 <987061450@qq.com>
2023-06-24 13:12:08 +0800

    init

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+ * @Author: 杨雪枫 987061450@qq.com
+ * @Date: 2023-06-24 12:53:59
+ * @LastEditors: 杨雪枫 987061450@qq.com
+ * @LastEditTime: 2023-06-24 12:55:16
+ * @FilePath: /eslint-prettier-test/index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+// 数组去重
